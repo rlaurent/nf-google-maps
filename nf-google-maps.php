c520eb181e79fcc58deb5d483f0ff8f8af5c9bf2
Romain Laurent <rlaurent89@gmail.com>
2018-06-12 18:12:48 +0200

    Add Google Maps Place API JS Script, Begin HTML template for the autocomplete input

--- nf-google-maps.php
+++ nf-google-maps.php
@@ -8,55 +8,59 @@ Author: Romain Laurent
 Author URI: https://rlaurent.com
 */
 
-// Some define
+/**
+ * Vars
+ */
 define( 'NF_GOOGLE_MAPS_VERSION', '1.0.0' );
 define( 'NF_GOOGLE_MAPS_AUTHOR', 'Romain Laurent' );
 
-// Include the dependencies needed to instantiate the plugin.
+/**
+ * Include the dependencies needed to instantiate the plugin.
+ */
 foreach ( glob( plugin_dir_path( __FILE__ ) . 'admin/*.php' ) as $file ) {
     include_once $file;
 }
 
-// Create the admin setting page
-add_action( 'plugins_loaded', 'nf_google_maps_custom_admin_settings' );
 /**
- * Starts the plugin.
- *
- * @since 1.0.0
+ * Create the admin page.
  */
 function nf_google_maps_custom_admin_settings() {
     $plugin = new NF_Google_Maps_Submenu( new NF_Google_Maps_Submenu_Page() );
     $plugin->init();
 }
+add_action( 'plugins_loaded', 'nf_google_maps_custom_admin_settings' );
 
-// Add filter for ninja-forms
-add_filter( 'ninja_forms_field_template_file_paths', 'nf_google_maps_template_file_path' );
-add_filter( 'ninja_forms_register_fields', 'nf_google_maps_register_fields');
+/**
+ * Add Google Maps API Places with Key
+ */
+function nf_google_maps_scripts() {
+    wp_enqueue_script( 'google_maps_js', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCd4goTP3cAVKbIgn4OOUqz7ucDmekfwKI&libraries=places', '', '' );
+}
+add_action( 'wp_enqueue_scripts', 'nf_google_maps_scripts' );
+
 
 /**
  * Register our template
- * 
- * @param  array
- * @return array
  */
 function nf_google_maps_template_file_path( $paths ){
     //look for our templates in the same directory as the plugin
     $paths[] = trailingslashit( __DIR__ . "/templates" );
     return $paths;
 }
+add_filter( 'ninja_forms_field_template_file_paths', 'nf_google_maps_template_file_path' );
 
 /**
 * Register our field
-*
-* @param array $actions
-* @return array
 */
 function nf_google_maps_register_fields($actions){
-    $actions[ 'ninjaformsgooglemaps' ] = new NF_Google_Maps();
+    $actions[ 'google-maps' ] = new NF_Google_Maps();
     return $actions;
 }
+add_filter( 'ninja_forms_register_fields', 'nf_google_maps_register_fields');
 
-// Get base classes from ninja-forms
+/**
+ * Get base classes from ninja-forms
+ */
 require_once plugin_dir_path( __FILE__ ) . "../ninja-forms/includes/Abstracts/Field.php";
 require_once plugin_dir_path( __FILE__ ) . "../ninja-forms/includes/Abstracts/Input.php";
 require_once plugin_dir_path( __FILE__ ) . "../ninja-forms/includes/Fields/Textbox.php";
@@ -65,18 +69,15 @@ require_once plugin_dir_path( __FILE__ ) . "../ninja-forms/includes/Fields/Textb
  * Class NF_Google_Maps
  */
 class NF_Google_Maps extends NF_Fields_Textbox {
-    protected $_name = 'ninjaformsgooglemaps';
+    protected $_name = 'google-maps';
     protected $_section = 'common'; // which drawer to place field
     protected $_icon = 'map-o'; // name of a Font Awesome ICON
-    protected $_type = 'ninjaformsgooglemaps';
-    protected $_templates = 'ninjaformsgooglemaps'; // which template to use
+    protected $_type = 'google-maps';
+    protected $_templates = 'google-maps'; // which template to use
+
     public function __construct() {
         parent::__construct();
         $this->_nicename = __( 'Google Maps Address', 'ninja-forms' ); //Nice name for the field
     }
 }
-
-
-
-
 
