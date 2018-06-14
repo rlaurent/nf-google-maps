cbbccfda332f9d45eb0d0b24e854bb1f7511ab14
Romain Laurent <rlaurent89@gmail.com>
2018-06-14 10:01:19 +0200

    fix input value was not submitted

--- js/nf-google-maps.js
+++ js/nf-google-maps.js
@@ -1,4 +1,9 @@
-jQuery(document).ajaxComplete(function (event, jqXHR, ajaxOptions) {
-   var input = document.getElementById('nf-field-google-maps');
-   autocomplete = new google.maps.places.Autocomplete(input);
+jQuery(document).on( 'nfFormReady', function( e, layoutView ) {
+  var input = document.getElementById('nf-field-google-maps');
+  autocomplete = new google.maps.places.Autocomplete(input, {types: ['geocode']});
+  autocomplete.addListener('place_changed', fillInAddress);
+
+  function fillInAddress(){
+    jQuery( '#nf-field-google-maps' ).val( autocomplete.getPlace().formatted_address ).change();
+  }
 });
